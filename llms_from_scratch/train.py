e43c5dea62c48d31813091cb6fefdef8267c3dc9
zhujinchang <stdzhu@gmail.com>
2025-11-21 12:37:15 +0800

    Finished.

---
+++ llms_from_scratch/train.py
@@ -0,0 +1,54 @@
+import torch
+from .utility import calc_loss_loader, calc_loss_batch, text_to_token_ids, token_ids_to_text
+from .generate import generate_text_simple
+
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval() #A
+    with torch.no_grad(): #B
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter) #C
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model, idx=encoded,
+            max_new_tokens=50, context_size=context_size
+        )
+        decoded_text = token_ids_to_text(token_ids, tokenizer)
+        print(decoded_text.replace("\n", " "))  # Compact print format
+    model.train()
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs,
+                       eval_freq, eval_iter, start_context):
+    train_losses, val_losses, track_tokens_seen = [], [], [] #A
+    tokens_seen, global_step = 0, -1
+ 
+    for epoch in range(num_epochs): #B
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad() #C
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward() #D
+            optimizer.step() #E
+            tokens_seen += input_batch.numel()
+            global_step += 1
+ 
+            if global_step % eval_freq == 0: #F
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+ 
+        generate_and_print_sample(  #G
+            model, train_loader.dataset.tokenizer, device, start_context
+        )
+    return train_losses, val_losses, track_tokens_seen
---
+++ llms_from_scratch/utility.py
@@ -0,0 +1,32 @@
+import torch
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1), target_batch.flatten()
+    )
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if num_batches is None:
+        num_batches = len(data_loader) #A
+    else:
+        num_batches = min(num_batches, len(data_loader)) #B
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item() #C
+        else:
+            break
+    return total_loss / num_batches #D
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimensio
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
